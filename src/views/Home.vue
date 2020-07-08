aef8be8ea7b9d66d16a06851b43ace445aa842f3
yaya <1914276342@qq.com>
2020-07-08 17:20:13 +0800

    222

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App!1111"/>
+    <HelloWorld msg="Welcome to Your Vue.js App! LuckyRiko!"/>
   </div>
 </template>
 
